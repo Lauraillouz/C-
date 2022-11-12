f05b68d045adf6e7d17ba57fb0d839b2d27acef7
Laura Illouz <lauraillouz.contact@gmail.com>
2022-11-12 14:23:54 +0100

    delegates

---
+++ 16. Events and Delegates/Delegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Pierre", "Paul", "Jacques" };
+
+            Console.WriteLine("---before---");
+            // print the names before the remove all method
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            // call filtering method that is needed for RemoveAll delegate
+            // the method is passed as a parameter
+            names.RemoveAll(FilterContainsI);
+
+            Console.WriteLine("---after---");
+            // print the names after remove all method
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+
+        static bool FilterContainsI(string s)
+        {
+            // returns whether the string contains the letter i
+            return s.Contains("i");
+        }
+    }
+}
